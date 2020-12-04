316261b2969638d99042c8a31b8b2df85319c0c2
jakemeout <jacobosity@gmail.com>
2020-12-04 15:09:01 -0500

    Added README

---
+++ Frontend/src/components/Searchbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Searchbar = () => {
+  return (
+      <>
+    <input
+      style={{width: "20rem",
+    background: "#F2F1F9",
+    border: "none",
+    padding: "0.5rem",
+    height: "20px"
+    }}
+    />
+    </>
+  );
+};
+
+export default Searchbar;
