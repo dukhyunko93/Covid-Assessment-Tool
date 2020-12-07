0e8d4ef966a3f17291279758d62e79a6cd56604e
dukhyunko93 <dukhyunko93@gmail.com>
2020-12-07 18:33:15 -0500

    search box

--- Frontend/src/components/Searchbar.js
+++ Frontend/src/components/Searchbar.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useRef }from "react";
+import SearchIcon from '@material-ui/icons/Search';
+import Autocomplete from './Autocomplete'
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+  background-color: #d1e0e0;
+  :hover {
+    background-color: #a3c2c2;
+  }
+  width: 50vh;
+  border-radius: 10px;
+  display: flex;
+  margin: 10px;
+  padding: 5px;
+  align-items: center;
+`
+const SearchBox = styled(Autocomplete)`
+`
+const Icon = styled(SearchIcon)`
+  margin-right: 5px;
+`
 
 const Searchbar = () => {
+
   return (
-      <>
-    <input
-      style={{width: "20rem",
-    background: "#F2F1F9",
-    border: "none",
-    padding: "0.5rem",
-    height: "20px"
-    }}
-    />
-    </>
-  );
+    <Wrapper>
+      <div>
+        <Icon />
+      </div>
+        <SearchBox></SearchBox>
+    </Wrapper>
+  )
 };
 
 export default Searchbar;
+
+
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -18,5 +18,3 @@ ReactDOM.render(
 reportWebVitals();
 
 
-
-
